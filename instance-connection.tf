cb9a88e7f61c09470b0399f6d3548aceb12efda6
ahmedovelshan <80003705+ahmedovelshan@users.noreply.github.com>
2024-07-16 19:12:58 +0400

    Update instance-connection.tf

--- instance-connection.tf
+++ instance-connection.tf
@@ -18,19 +18,46 @@ resource "aws_security_group" "ec2-endpoint-connection" {
         from_port = 0
         to_port = 0
         cidr_blocks = [ "0.0.0.0/0" ]
-    }    
+    }
     egress {
         protocol = "TCP"
         from_port = 22
         to_port = 22
-        cidr_blocks = [aws_security_group.alb-to-web.id]
+        cidr_blocks = [ "10.0.1.0/24", "10.0.2.0/24" ]
     }
 
 }
 
 
+resource "aws_security_group" "alb-to-web" {
+    vpc_id              = aws_vpc.devops-vpc.id
+    name                = "alb-to-web"
+    description         = "Access from ALB to WEB subnet"
+    dynamic "ingress" {
+        for_each = var.port-1
+        content {
+          protocol = "tcp"
+          from_port = ingress.value
+          to_port = ingress.value
+          cidr_blocks = var.public-subnet-cidr
+        }     
+    }
+    ingress {
+        protocol = "TCP"
+        from_port = 22
+        to_port = 22
+        security_groups = [aws_security_group.ec2-endpoint-connection.id]
+    }	
+    egress {
+        protocol = "-1"
+        from_port = 0
+        to_port = 0
+        cidr_blocks = [ "0.0.0.0/0" ]
+    }
+}
 
 resource "aws_ec2_instance_connect_endpoint" "for-resource-connection" {
   subnet_id = aws_subnet.ec2-endpoint-subnet.id
+  security_group_ids = [aws_security_group.ec2-endpoint-connection.id]
 }
 
